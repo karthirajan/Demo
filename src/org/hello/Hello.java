2cfdd5f3dc67fa3243b0e9ddccea90a96d4bdfa0
karthirajan <kajekarthi@gmail.com>
2019-03-02 12:50:12 +0530

    modified

--- src/org/hello/Hello.java
+++ src/org/hello/Hello.java
@@ -2,8 +2,20 @@ package org.hello;
 
 public class Hello {
 	public void x() {
-		// TODO Auto-generated method stub
+		System.out.println("karthi");
 
+	}
+	public void y() {
+		System.out.println("git class");
+
+	}
+	public void z() {
+		System.out.println("saturday");
+
+	}
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		h.x();
 	}
 
 }
